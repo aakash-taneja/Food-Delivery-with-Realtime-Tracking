9ae4264628d65f6ecb539984fad9b061ced222eb
Aakash Taneja <56475445+aakash-taneja@users.noreply.github.com>
2022-10-23 22:59:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const passport = require('passport')
 const Emitter = require('events')
 
 // Database connection
-mongoose.connect("mongodb+srv://pizza-app:pizzaboy@cluster0.jl3nmds.mongodb.net/momo-app?retryWrites=true&w=majority", {useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true});
+mongoose.connect("mongodb+srv://pizza-app:pizzaboy@cluster0.jl3nmds.mongodb.net/pizza?retryWrites=true&w=majority", {useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true});
 const connection=mongoose.connection;
 connection.once('open',()=>{
     console.log("Database connected....");
