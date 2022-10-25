400a8013666f7aa39d0cc03a59c0dd5e6557461c
aakash-taneja <19uec168@lnmiit.ac.in>
2022-10-25 16:19:54 +0530

    updated commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const passport = require('passport')
 const Emitter = require('events')
 
 // Database connection
-mongoose.connect("mongodb://localhost:27017/pizza", {useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true});
+mongoose.connect("mongodb+srv://pizza-app:pizzaboy@cluster0.jl3nmds.mongodb.net/pizza?retryWrites=true&w=majority", {useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true,useFindAndModify:false});
 const connection=mongoose.connection;
 connection.once('open',()=>{
     console.log("Database connected....");
